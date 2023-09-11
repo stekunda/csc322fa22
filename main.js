5903686b8c85343be382fe5f1866a3e39896a740
Stekunda Henry <stekundah@gmail.com>
2023-09-11 19:37:54 -0400

    added tetrahedron

--- main.js
+++ main.js
@@ -10,23 +10,43 @@ const camera = new THREE.PerspectiveCamera(
     1,
     500
 );
-camera.position.set(0, 0, 100);
+camera.position.set(0, 0, 0);
 camera.lookAt(0, 0, 0);
+camera.position.z = 100;
 
 const scene = new THREE.Scene();
 
 //create a blue LineBasicMaterial
-const material = new THREE.LineBasicMaterial({ color: 0x00ff00 });
+const material = new THREE.LineBasicMaterial({
+    color: 0x00ff00,
+    depthTest: true,
+});
 
 const points = [];
 points.push(new THREE.Vector3(-10, 0, 0));
 points.push(new THREE.Vector3(0, 10, 0));
+points.push(new THREE.Vector3(0, 0, 10));
 points.push(new THREE.Vector3(10, 0, 0));
+points.push(new THREE.Vector3(0, 0, -10));
+points.push(new THREE.Vector3(0, 10, 0));
+points.push(new THREE.Vector3(0, 0, -10));
 points.push(new THREE.Vector3(-10, 0, 0));
+points.push(new THREE.Vector3(0, 0, 10));
+points.push(new THREE.Vector3(0, 10, 0));
+points.push(new THREE.Vector3(10, 0, 0));
 
 const geometry = new THREE.BufferGeometry().setFromPoints(points);
 
 const line = new THREE.Line(geometry, material);
 
 scene.add(line);
-renderer.render(scene, camera);
+// renderer.render(scene, camera);
+
+function animate() {
+    requestAnimationFrame(animate);
+    line.rotation.x += 0.01;
+    line.rotation.y += 0.01;
+    renderer.render(scene, camera);
+}
+
+animate();
